235202d85ff69df73ce5ad3af13a1241360b3026
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-17 09:15:15 +0530

    Building: BookStore MERN Project

--- MERN_Projects/BookStore/backend/index.js
+++ MERN_Projects/BookStore/backend/index.js
@@ -1,61 +1,33 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS Policy
+// Option 1: Allow all origins with default of cors(*)
+ app.use(cors());
+
+// Option 2: Allow custom origins
+// app.use(
+//     cors({
+//         origin: "http://localhost:3000",
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// )
+
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send("Welcome to MERN stack tutorial");
 });
 
-// Route for save a new book
-app.post('/books', async (request, response) => {
-    try {
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields: title, author, publishYear',
-            });
-        }
-
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-// Route for get all books from database
-
-app.get('/books', async (request, response) => {
-    try {
-        const books = await Book.find({});
-        return response.status(200).json({
-            count: books.length,
-            data: books
-        });
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-    }
-})
+app.use('/books', booksRoute);
 
 mongoose
    .connect(mongoDBURL)
