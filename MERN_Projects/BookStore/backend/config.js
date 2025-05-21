92a3ce63ec23e34adc950f701a4b2ebf03555b25
satyamXcode <jaiswalsatyam876@gmail.com>
2025-05-21 06:36:31 +0530

    Added pages code

--- MERN_Projects/BookStore/backend/config.js
+++ MERN_Projects/BookStore/backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://jaiswalsatyam876:Tm9PwpbXosM0NpZe@cluster0.iyyxmta.mongodb.net/books-collection';
+export const mongoDBURL = 'mongodb+srv://jaiswalsatyam876:Tm9PwpbXosM0NpZe@cluster0.iyyxmta.mongodb.net/';
