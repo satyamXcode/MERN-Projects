2b1321c12656297166727fe15fcea5b01917fc8c
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-09 13:03:24 +0530

    Completed Some Code for Backend & MongoDb database

--- MERN_Projects/Brainly/src/index.ts
+++ MERN_Projects/Brainly/src/index.ts
@@ -5,7 +5,6 @@ import { ContentModel, UserModel } from "./db";
 import { userMiddleware } from "./authMiddleware";
 import { JWT_PASSWORD } from "./config";
 
-
 const app = express();
 app.use(express.json());
 
@@ -56,22 +55,49 @@ app.post("/api/v1/signin", async (req, res) => {
 });
 
 app.post("/api/v1/content", userMiddleware, async (req, res) => {
-       const link = req.body.link;
-       const type = req.body.type;
+  const link = req.body.link;
+  const type = req.body.type;
 
-       await ContentModel.create({
-        link,
-        type,
-        //@ts-ignore
-        userId: req.userId,
-        tags: []
-       })
+  await ContentModel.create({
+    link,
+    type,
+    //@ts-ignore
+    userId: req.userId,
+    tags: [],
+  });
 
-       res.json({
-         message: "Content added"
-       })
+  res.json({
+    message: "Content added",
+  });
 });
 
-app.get("/api/v1/content", (req, res) => {});
+app.get("/api/v1/content", userMiddleware, async (req, res) => {
+  // @ts-ignore
+  const userId = req.userId;
+  const content = await ContentModel.find({
+    userId: userId,
+  }).populate("userId", "username");
+  res.json({
+    content,
+  });
+});
+
+app.delete("/api/v1/content", userMiddleware, async (req, res) => {
+  const contentId = req.body.contentId;
+
+  await ContentModel.deleteMany({
+    contentId,
+    // @ts-ignore
+    userId: req.userId,
+  });
+
+  res.json({
+    message: "Deleted",
+  });
+});
+
+app.post("/api/v1/brain/share", (req, res) => {});
+
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
 
 app.listen(3000);
