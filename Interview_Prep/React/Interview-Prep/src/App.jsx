117ec040ddc85eda8367d818ab38b7f08beddb75
satyamXcode <jaiswalsatyam876@gmail.com>
2025-06-17 07:46:26 +0530

    Inline Style Css

--- Interview_Prep/React/Interview-Prep/src/App.jsx
+++ Interview_Prep/React/Interview-Prep/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Counter from './components/Counter'
+import ColorStyles from './components/ColorStyles'
  
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
        <Counter />
+       <ColorStyles />
     </>
   )
 }
---
+++ Interview_Prep/React/Interview-Prep/src/components/ColorStyles.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ColorStyles = () => {
+  return (
+    <div style={{ color: "red", fontSize: "20px"}}>Hello, world!</div>
+  )
+}
+
+export default ColorStyles
